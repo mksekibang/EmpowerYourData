06a0ae11f515cacae91b8f66d1746f30f941dd36
mksekibang <dmitryshostakovich@gmail.com>
2012-07-15 20:48:18 +0900

    gassyuku!!

--- SnakeBBS/helloworld.py
+++ SnakeBBS/helloworld.py
@@ -1,6 +1,7 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-
+import os
+from google.appengine.ext.webapp import template
 
 class MainPage(webapp.RequestHandler):
     
@@ -8,7 +9,7 @@ class MainPage(webapp.RequestHandler):
     def get(self):
         self.layout['content'] = self.htmldir + 'write.html'
         params = {'layout':self.layout,'form':bbsform()}
-        fpath ~ os.pathjoin(os.path.dirname(_file_),'layouts',self.layout_file)
+        fpath = os.pathjoin(os.path.dirname(_file_),'layouts',self.layout_file)
         html = template.render(fpath.params)
         self.response.out.write(html) 
 
