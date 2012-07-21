c398be020be7ce8b8d3012866e4e108fdea2b6a5
mksekibang <dmitryshostakovich@gmail.com>
2012-07-21 17:51:16 +0900

    画面ができました。

--- LocEmpowerYourData/helloworld.py
+++ LocEmpowerYourData/helloworld.py
@@ -1,14 +1,17 @@
+#!-*- coding:utf-8 -*-"
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 import os
 from google.appengine.ext.webapp import template
+import bbsdata
 
 class MainPage(webapp.RequestHandler):
     
     
     def get(self):
+        params = {'form':bbsdata.bbsform()}
         fpath = os.path.join(os.path.dirname(__file__),'htmldir','write.html')
-        html = template.render(fpath,None)
+        html = template.render(fpath,params)
         self.response.out.write(html)
 
 
